177bb094b278aafd084335a81cd023a6388216e3
Fezzopro <fezzopro@gmail.com>
2023-05-11 01:10:22 +0200

    Add calculator module unit tests

---
+++ tests/calulator.test.js
@@ -0,0 +1,31 @@
+import calculator from "../modules/calculator";
+describe('Calculator module Test suit', () => {
+  test('add', () => {
+    const newAdd = () => { calculator.add('r', 4); };
+
+    expect(newAdd).toThrow(new Error('Invalid Number'));
+    expect(calculator.add(5, 4)).toBe(9);
+
+  });
+  
+  test('subtract', () => {
+    const newSubstract = () => { calculator.subtract('r', 4); };
+
+    expect(newSubstract).toThrow(new Error('Invalid Number'));
+    expect(calculator.subtract(5, 4)).toBe(1);
+  });
+
+  test('divide', () => {
+    const newDivide = () => { calculator.divide('r', 4); };
+
+    expect(newDivide).toThrow(new Error('Invalid Number'));
+    expect(calculator.multiply(5, 4)).toBe(20);
+  });
+
+  test('multiply', () => {
+    const newMultiply = () => { calculator.multiply('r', 4); };
+
+    expect(newMultiply).toThrow(new Error('Invalid Number'));
+    expect(calculator.divide(5, 4)).toBe(1.25);
+  });
+});
